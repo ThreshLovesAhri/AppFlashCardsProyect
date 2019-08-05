447e5c91d044cf4c318f8060e8e38ab906b9f662
Jose Aguilar <reyox009@gmail.com>
2019-08-04 20:35:47 -0600

    Prueba de cambio de Botones, cambio pantalla

--- app/src/main/java/com/example/flashcardssumas/SumasTarjetas.kt
+++ app/src/main/java/com/example/flashcardssumas/SumasTarjetas.kt
@@ -1,8 +1,10 @@
 package com.example.flashcardssumas
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import kotlinx.android.synthetic.main.activity_main.*
 
 import kotlinx.android.synthetic.main.activity_sumas_tarjetas.*
 
@@ -16,6 +18,10 @@ class SumasTarjetas : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+        BtnSalir.setOnClickListener {
+            val into1= Intent(this, MainActivity::class.java)
+            startActivity(into1)
         }
     }
 
